b701366c6bbc8cbab39d2792d838e4b858900ed9
Kusum <Kusum-Thapa@users.noreply.github.com>
2020-12-21 10:43:00 +0000

    Added Landing Page and Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from "react"
+import './App.css'
+import AppNavBar from "./components/structure/AppNavBar"
 import './App.css';
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <AppNavBar />
+      <Typography align="center">
+                <div id="content-wrapper" className="mui--text-center">
+                    <h1>Help Who Needs</h1>
+                    <p>
+                        Our TechForGood project aims to match a volunteer with a vulnerable
+                        person who is shielding in these unprecedented times. Hence our
+                        application is called HelpWhoNeeds. The app will aim to match a
+                        volunteer with a vulnerable person in their immediate local area
+                        helping with chores such as shopping, GP visits, prescriptions, dog
+                        walking or just some social contact with the appropriate measures in
+                        place. We hope this app brings some comfort and relief to the users
+                        with a sense of moral and social obligation.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+                    <Button
+                        variant="outlined"
+                        color="default"
+                        onClick={() => {
+                            alert("This works on every component!");
+                        }}
+                    >
+                        I need Help
+        </Button>
+                    <Button
+                        variant="outlined"
+                        color="default"
+                        onClick={() => {
+                            alert("This works on every component!");
+                        }}
+                    >
+                        I can Help
+        </Button>n
+                </div>
+            </Typography>
+            <footer>
+                <div>
+                    Made by <a href="https://github.com/YourTechAngels">YourTechAngels</a>
+                </div>
+            </footer>
     </div>
   );
 }
